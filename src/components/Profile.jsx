142f63d89a5359cbb561baa81f46a80160d2a5ba
DeveloperTitarenko <DeveloperTitarenko@gmail.com>
2020-07-30 22:13:52 +0300

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>New post</div>
+            </div>
+            <div>Post 1</div>
+            <div>Post 3</div>
+        </div>
+        )
+}
+export default Profile;
+
+
